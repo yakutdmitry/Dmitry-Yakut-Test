818596858f587b89943aec2e8317969735ce47d2
yakutdmitry <D23125246@mytudublin.ie>
2024-05-02 14:42:41 +0100

    Everything works

--- Starter Code/Player.gd
+++ Starter Code/Player.gd
@@ -6,22 +6,16 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		
+		$Sprite2D2.position = $".".position
 		move_and_slide()
 	if is_on_floor():
-		inst(true, Vector2(0, 400))
+		inst(true)
 		queue_free()
 		
-	
-#func _inst():
-	#var instance = explosion_scene.instantiate()
-	#instance.global_position = global_position
-	#instance.emitting = true
-	#add_child(instance)
-	#
-func inst(on, pos):
+
+func inst(on):
 	var explosion = explosion_scene.instantiate()
-	explosion.position = pos
+	explosion.position = $Sprite2D2.position
 	explosion.emitting = on
 	$"..".add_child(explosion)
 
