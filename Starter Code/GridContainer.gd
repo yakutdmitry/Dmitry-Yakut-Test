b9a3cd4fa78144f86b731b796626a74c60d5a4e6
yakutdmitry <D23125246@mytudublin.ie>
2024-05-02 14:36:17 +0100

    blocks are spawning, explosions almost work

--- Starter Code/GridContainer.gd
+++ Starter Code/GridContainer.gd
@@ -1,5 +1,24 @@
 extends GridContainer
 
+@export var blocks:PackedScene 
+var num_blocks = 1
 
 func _on_h_slider_value_changed(value):
 	$"../RichTextLabel".text = str(value)
+	num_blocks = value
+
+
+
+
+func _on_button_button_down():
+	if(num_blocks != 0):
+		for i in range(num_blocks):
+			var posx = cos((2 * PI / num_blocks) * i) * 350
+			var posy = sin((2 * PI / num_blocks) * i) * 350
+			_inst(Vector2(posx, posy))
+			print(posx)
+	
+func _inst(pos):
+	var instance = blocks.instantiate()
+	instance.position = pos
+	$"../../pivot".add_child(instance)
--- Starter Code/Player.gd
+++ Starter Code/Player.gd
@@ -6,14 +6,22 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
 		move_and_slide()
 	if is_on_floor():
+		inst(true, Vector2(0, 400))
 		queue_free()
-		_inst()
+		
 	
-func _inst():
-	var instance = explosion_scene.instantiate()
-	instance.global_position = global_position
-	instance.emitting = true
-	add_child(instance)
-	
+#func _inst():
+	#var instance = explosion_scene.instantiate()
+	#instance.global_position = global_position
+	#instance.emitting = true
+	#add_child(instance)
+	#
+func inst(on, pos):
+	var explosion = explosion_scene.instantiate()
+	explosion.position = pos
+	explosion.emitting = on
+	$"..".add_child(explosion)
+
